89a4bec8abec4f294146a955d6ab8befe9b4abde
Richard Newman <newman_richard@outlook.com>
2020-07-11 17:18:22 +1000

    Update script to install https dev cert in root store

--- Scripts/installSelfSignedCert.ps1
+++ Scripts/installSelfSignedCert.ps1
@@ -7,7 +7,7 @@ $pfxpath = Join-Path $scriptPath $pfxname
 Add-Type -AssemblyName System.Security
 $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
 $cert.Import($pfxpath, $password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]"PersistKeySet")
-$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
+$store = New-Object System.Security.Cryptography.X509Certificates.X509Store -argumentlist "root", CurrentUser
 $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]"ReadWrite")
 $store.Add($cert)
 $store.Close()
